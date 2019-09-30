3d8641e572ea9f6e37feffe50f08360446f431a3
Aislan Jefferson <aislanjsb@gmail.com>
2019-09-30 18:05:11 +0100

    Adicionado print de tempo decorrido

--- q03/ex3_p.c
+++ q03/ex3_p.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
 #include <sys/types.h>
 #include <sys/wait.h>
+
 int main() {
     pid_t pid = getpid();
+    struct timespec before, after;
     pid_t childpid;
+    clock_gettime(CLOCK_MONOTONIC, &before);
+
     for (int i = 0; i < 100; ++i) {
-        if((childpid =  fork()) == 0){
-            printf("Sou filho!");
+        if ((childpid = fork()) == 0) {
+            printf("Sou filho!\n");
             exit(0);
-        }else if(childpid > 0){
-           wait(&childpid);
+        } else if (childpid > 0) {
+            wait(&childpid);
         }
     }
-    printf("Sou pai! %d",pid);
-    char i = getchar(); // soh pra fazer esperar
+    printf("Sou pai! %d\n", pid);
+    clock_gettime(CLOCK_MONOTONIC, &after);
+    long msec_diff = (after.tv_sec - before.tv_sec) * 1000 + (after.tv_nsec - before.tv_nsec) / 1000000;
+    printf("Took %ld ms", msec_diff);
     return 0;
 }
--- q03/ex3_t.c
+++ q03/ex3_t.c
@@ -4,23 +4,25 @@
 #include <sys/stat.h>
 
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <pthread.h>
-void *printa()
-{
-printf("Sou filho!");
 
-pthread_exit(0);
+void *printa() {
+    printf("Sou filho!\n");
+
+    pthread_exit(0);
 }
 
 int main() {
     pid_t pid = getpid();
+    struct timespec before, after;
+    clock_gettime(CLOCK_MONOTONIC, &before);
     pthread_t thread;
     for (int i = 0; i < 100; ++i) {
         pthread_create(&thread, NULL, printa, NULL);
         pthread_join(thread, NULL);
     }
-    printf("Sou pai! %d",pid);
-    char i = getchar(); // soh pra fazer esperar
-    return 0;
+    printf("Sou pai! %d\n", pid);
+    clock_gettime(CLOCK_MONOTONIC, &after);
+    long msec_diff = (after.tv_sec - before.tv_sec) * 1000 + (after.tv_nsec - before.tv_nsec) / 1000000;
+    printf("Took %ld ms", msec_diff);
 }
