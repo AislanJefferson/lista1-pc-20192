25b564afc1a63b023c19a1b090b9502037ed97d3
Aislan Jefferson <aislanjsb@gmail.com>
2019-10-09 14:56:01 +0100

    Modificado para sleep e corrigido espera

--- q03/ex3_p.c
+++ q03/ex3_p.c
@@ -10,18 +10,20 @@ int main(int argc, char *argv[]) {
     int processes_qty = argc == 2 ? atoi(argv[1]) : 100;
     pid_t pid = getpid();
     struct timespec before, after;
-    pid_t childpid;
+    pid_t childpid[processes_qty];
     clock_gettime(CLOCK_MONOTONIC, &before);
 
     for (int i = 0; i < processes_qty; ++i) {
-        if ((childpid = fork()) == 0) {
-            printf("Sou filho! %d\n",i);
+        if ((childpid[i] = fork()) == 0) {
+            printf("Sou filho! %d\n", i);
+            sleep(10);
             exit(0);
-        } else if (childpid > 0) {
-            wait(&childpid);
         }
     }
     printf("Sou pai! %d\n", pid);
+    for (int i = 0; i < processes_qty; ++i) {
+        wait(&childpid[i]);
+    }
     clock_gettime(CLOCK_MONOTONIC, &after);
     long msec_diff = (after.tv_sec - before.tv_sec) * 1000 + (after.tv_nsec - before.tv_nsec) / 1000000;
     printf("Took %ld ms", msec_diff);
--- q03/ex3_t.c
+++ q03/ex3_t.c
@@ -10,6 +10,7 @@
 void *printa(void *arg) {
     printf("Sou filho! %d\n", *((int *) arg));
     free(arg);
+    sleep(10);
     pthread_exit(0);
 }
 
@@ -18,15 +19,18 @@ int main(int argc, char *argv[]) {
     pid_t pid = getpid();
     struct timespec before, after;
     clock_gettime(CLOCK_MONOTONIC, &before);
-    pthread_t thread;
+    pthread_t thread[threads_qty];
     for (int i = 0; i < threads_qty; ++i) {
         int *param = malloc(sizeof(i));
         *param = i;
-        pthread_create(&thread, NULL, printa, param);
-        pthread_join(thread, NULL);
+        pthread_create(&thread[i], NULL, printa, param);
     }
     printf("Sou pai! %d\n", pid);
+    for (int i = 0; i < threads_qty; ++i) {
+        pthread_join(thread[i], NULL);
+    }
     clock_gettime(CLOCK_MONOTONIC, &after);
     long msec_diff = (after.tv_sec - before.tv_sec) * 1000 + (after.tv_nsec - before.tv_nsec) / 1000000;
-    printf("Took %ld ms", msec_diff);
+    printf("Took %ld ms\n", msec_diff);
 }
+
