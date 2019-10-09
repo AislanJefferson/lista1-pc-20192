2bda92e1aee648504aa7b3718465f4ff48989a15
Aislan Jefferson <aislanjsb@gmail.com>
2019-10-09 20:33:37 +0100

    Corrigo flag de compilacao pthread

--- q03/build_start.sh
+++ q03/build_start.sh
@@ -2,7 +2,7 @@
 echo "Compilando o codigo fonte..."
 echo "- Compilando ex3_p"
 gcc ex3_p.c -o ex3_p
-gcc ex3_t.c -o ex3_t
+gcc -pthread ex3_t.c -o ex3_t
 echo "Executando os binarios"
 echo " "
 /usr/bin/time ./ex3_p $1
